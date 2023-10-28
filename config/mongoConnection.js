6a2b9e07290dbbcaa15aad3ab300d07ef1dbdabe
dbnjn <dbnjngoswami@gmail.com>
2023-10-28 19:05:44 +0530

    added kafka related files

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -5,11 +5,30 @@ const CONNECTION = process.env.CONNECTION;
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(CONNECTION);
+        await mongoose.connect(CONNECTION, {
+  serverSelectionTimeoutMS: 30000, // Adjust the timeout value as needed
+        });
+        const db = mongoose.connection;
+
+        // Event listeners for checking the connection
+        db.on('error', (error) => {
+          console.error('MongoDB connection error:', error);
+        });
+        
+        db.once('open', () => {
+          console.log('Connected to MongoDB!');
+        });
+        
+        db.on('disconnected', () => {
+          console.log('MongoDB disconnected.');
+        });
+
     } catch (error) {
         throw error
     }
 }
+
+
 
 module.exports = { connectToDB };
 
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,62 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const { Kafka,  Partitioners } = require('kafkajs')
 
-router.get('/allUsers', (req, res) => {
-    User.find({}).then((users) => {
+const kafka = new Kafka({
+    clientId: 'kafka-nodejs-starter',
+    brokers: ['localhost:9092'],
+  });
+
+  const producer = kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner,
+  })
+  
+// Send an event to the demoTopic topic
+
+
+// Disconnect the producer once we're done
+
+
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+
+
+
+
+router.get('/allUsers', async(req, res) => {
+     await consumer.connect()
+    await consumer.subscribe({ topic: 'notifications', fromBeginning: true })
+   
+    User.find({}).then(async(users) => {
         res.json(users);
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+              console.log({
+                value: message.value.toString(),
+              })
+            },
+          });
     }).catch((err) => {
         throw err
     })
 })
 
-router.post('/addUser',function(req, res){
+router.post('/addUser',async function(req, res){
+    await producer.connect()
+   
+// Connect to the producer
     const new_user = req.body;
     const user_instance = new User(new_user);
-    user_instance.save().then((newUser) => {
+    user_instance.save().then(async(newUser) => {
         res.json("New User has been added");
+        await producer.send({
+            topic: 'notifications',
+            messages: [
+              { value: newUser.name },
+            ],
+          });
+          await producer.disconnect();
     }).catch((err) => {
         throw err
     })   
